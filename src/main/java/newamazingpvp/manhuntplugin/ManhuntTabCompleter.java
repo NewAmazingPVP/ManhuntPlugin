1f6f99c5c48d1d65f9693581673a0a4798dd72d7
NewAmazingPVP <gamingofpat@gmail.com>
2024-11-09 17:55:44 -0500

    Update ManhuntTabCompleter.java

--- src/main/java/newamazingpvp/manhuntplugin/ManhuntTabCompleter.java
+++ src/main/java/newamazingpvp/manhuntplugin/ManhuntTabCompleter.java
@@ -12,10 +12,14 @@ import java.util.List;
 
 public class ManhuntTabCompleter implements TabCompleter {
 
-    private static final List<String> COMMANDS = Arrays.asList("start", "end", "list", "compass", "regen", "add", "remove", "piglindrop", "setmaxhealth", "setresistance");
+    private static final List<String> COMMANDS = Arrays.asList("start", "end", "list", "compass", "regen", "add", "remove", "piglindrop", "setmaxhealth", "setresistance", "help", "quicksettings");
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            numbers.add(i);
+        }
         if (args.length == 1) {
             return filterSuggestions(COMMANDS, args[0]);
         } else if (args.length == 2) {
@@ -27,13 +31,13 @@ public class ManhuntTabCompleter implements TabCompleter {
                 case "setresistance":
                     return filterSuggestions(Arrays.asList("runner", "hunter"), args[1]);
                 case "piglindrop":
-                    return List.of("%");
+                    return numbers.stream().map(String::valueOf).toList();
                 default:
                     return Bukkit.getOnlinePlayers().stream().map(Player::getName).toList();
             }
         } else if (args.length == 3) {
             if (args[0].equalsIgnoreCase("setmaxhealth") || args[0].equalsIgnoreCase("setresistance")) {
-                return List.of("<value>");
+                return numbers.stream().map(String::valueOf).toList();
             } else {
                 return Bukkit.getOnlinePlayers().stream().map(Player::getName).toList();
             }
